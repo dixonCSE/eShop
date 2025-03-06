bc4ccffe51ee32da4c3b0ef006a78c9fdd529743
Dixon Halder <dixonhalder7@gmail.com>
2025-03-06 20:14:30 +0600

    Delete eShop-main/eShop-main directory

--- eShop-main/eShop-main/src/app/data/global-constants.ts
+++
@@ -1,5 +0,0 @@
-export class GlobalConstants {
-    public static apiBaseURL: string =
-        'https://ac.relationsupershop.com/ecom/api/';
-    public static assetsBaseURL: string = 'https://ac.relationsupershop.com/';
-}
--- eShop-main/eShop-main/src/app/pipe/truncate.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'truncate', standalone: true })
-export class TruncatePipe implements PipeTransform {
-    transform(value: string, limit: number = 20): string {
-        return value.length < limit ? value : value.slice(0, limit) + '...';
-    }
-}
